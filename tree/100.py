ab492a2229589d22e34137570c93254cad859054
lingqinx <lingqinx@usc.edu>
2017-10-11 16:34:17 -0700

    handle tree depth

---
+++ tree/100.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        #recersive : loop of tree
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        return p is q   #is will return True if two variables point to the same object, == if the objects referred to by the variables are equal.
+        
---
+++ tree/101.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        return self.isMirror(root.left,root.right)
+    
+    def isMirror(self,p,q):
+        if p == None and q== None:
+            return True
+        if p==None or q==None:
+            return False
+        return p.val == q.val and self.isMirror(p.left,q.right) and self.isMirror(p.right,q.left)
